be59627f4557ce4d8b9fdf487369caa04a5e2ab6
JackMartin00 <jmart303@uic.edu>
2021-11-12 22:05:53 -0600

    putExtra

--- app/src/main/java/com/example/splitty/scanConfirmation.java
+++ app/src/main/java/com/example/splitty/scanConfirmation.java
@@ -2,16 +2,19 @@ package com.example.splitty;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class scanConfirmation extends AppCompatActivity {
-
-    String numPeople = "";
+    TextView numPeople;
+    double totalAmount = 2892.05;
+    double soloAmount = 0.0;
+    int numPeepo = 0;
+    String numPeopleS = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,31 +23,35 @@ public class scanConfirmation extends AppCompatActivity {
         Button next = findViewById(R.id.button4);
         Button modify = findViewById(R.id.button3);
 
-
-        EditText et = findViewById(R.id.totalPeopleInput2);
-
-        et.setOnKeyListener(new View.OnKeyListener() {
-
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-
-                numPeople = String.valueOf(et.getText());
-                return true;
-            }
-        });
-
-
-
-
         modify.setOnClickListener(bruh->{
             Intent intent = new Intent(this, manual_Bill.class);
             startActivity(intent);
         });
 
         next.setOnClickListener(moment->{
-            Intent intent = new Intent(this, addFriends.class);
-            intent.putExtra("numPeople", numPeople);
-            startActivity(intent);
+            numPeople = findViewById(R.id.totalPeopleInput2);
+
+//            if(numPeopleS != "" || numPeopleS == null || !numPeopleS.matches(""))
+            numPeopleS = numPeople.getText().toString();
+
+//            if(numPeopleS != "" || numPeopleS == null || !numPeopleS.matches(""))
+            if(!numPeopleS.matches(""))
+                numPeepo = Integer.parseInt(numPeopleS);
+//            else
+//                numPeepo = 0;
+//            soloAmount = totalAmount / numPeepo;
+            if(numPeopleS == null || numPeepo == 0 || numPeepo == 1 || numPeopleS.matches("")) {
+                Toast.makeText(this, "Invalid total Number", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                soloAmount = totalAmount / numPeepo;
+                Log.i("checking total", String.valueOf(soloAmount));
+                Intent intent = new Intent(this, addFriends.class);
+                intent.putExtra("numPeople", numPeepo); //num of people
+                intent.putExtra("soloAmount", soloAmount);
+                startActivity(intent);
+            }
         });
     }
 
